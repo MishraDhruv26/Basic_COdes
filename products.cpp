df05df34007f898ac09aaed42ce432fd780ae5a4
Anurodh_aka_idabora <80338513+idabora@users.noreply.github.com>
2021-10-07 19:06:50 +0530

    Update products.cpp

--- products.cpp
+++ products.cpp
@@ -5,7 +5,7 @@ int main()
     int a,b;
     cout<<"Enter two Integers -"<<endl;
     cin>>a>>b;
-    cout<<"The Product of these two Integers are -";
+    cout<<"The Product of these two Integers are-";
     cout<<a*b;
     return 0;
 }
