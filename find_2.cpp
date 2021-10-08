0dc62c60593f58452da9b505f722d4da84daeca7
Anurodh_aka_idabora <80338513+idabora@users.noreply.github.com>
2021-10-08 22:19:17 +0530

    Update find_2.cpp

--- find_2.cpp
+++ find_2.cpp
@@ -3,9 +3,11 @@
 int main()
 {
     int a;
+    cout<<"Enter a integer-";
+    cin>>a;
     if (a%2==0)
-        cout<<"divisible";
+        cout<<"YES,User Entered Number is Divisible by 2";
     else
-        cout<<"not divisible";
+        cout<<"NO,User Entered Number is Not Divisible by 2";
     return 0;
 }
